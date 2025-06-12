92d0a72b794ec01e658eb1dabf78ae12881be6ee
Sanjib Kumar Mandal <sanjib0248@gmail.com>
2025-06-13 00:21:21 +0530

    updated header

--- src/app/components/code-preview/code-preview.component.ts
+++ src/app/components/code-preview/code-preview.component.ts
@@ -1,21 +1,9 @@
-import {
-  AfterViewInit,
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  effect,
-  ElementRef,
-  inject,
-  input,
-  PLATFORM_ID,
-  ViewChild,
-} from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-typescript';
-import { isPlatformBrowser } from '@angular/common';
-import { KageCopyDirective, KageIcon } from "kage-ui";
 
 @Component({
   selector: 'app-code-preview',
@@ -24,47 +12,29 @@ import { KageCopyDirective, KageIcon } from "kage-ui";
   styleUrl: './code-preview.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class CodePreviewComponent implements AfterViewInit {
-  @ViewChild('tsBlock', { static: true }) tsBlock!: ElementRef<HTMLElement>;
-  @ViewChild('htmlBlock', { static: true }) htmlBlock!: ElementRef<HTMLElement>;
-  @ViewChild('scssBlock', { static: true }) scssBlock!: ElementRef<HTMLElement>;
+export class CodePreviewComponent {
   scssCode = input<string>();
   htmlCode = input<string>();
   tsCode = input<string>();
   activeTab = 0;
 
-  private platformId = inject(PLATFORM_ID);
-
-  constructor() {
-    effect(() => this.highlight());
-  }
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.highlight();
-    }
-  }
-
-  highlight() {
-    if (isPlatformBrowser(this.platformId)) {
-      // HTML
-      if (this.htmlCode()) {
-        this.htmlBlock.nativeElement.innerHTML = Prism.highlight(this.htmlCode()!, Prism.languages['markup'], 'markup');
-      } else {
-        this.htmlBlock.nativeElement.innerHTML = '<p style="color: #865955;">No HTML</p>';
-      }
-      // SCSS 
-      if (this.scssCode()) {
-        this.scssBlock.nativeElement.innerHTML = Prism.highlight(this.scssCode()!, Prism.languages['scss'], 'scss');
-      } else {
-        this.scssBlock.nativeElement.innerHTML = '<p style="color: #865955;">No SCSS</p>';
-      }
-      // Typescript
-      if (this.tsCode()) {
-        this.tsBlock.nativeElement.innerHTML = Prism.highlight(this.tsCode()!, Prism.languages['typescript'], 'typescript');
-      } else {
-        this.tsBlock.nativeElement.innerHTML = '<p style="color: #865955;">No Typescript Code</p>';
-      }
+  highLight(type: 'html' | 'scss' | 'ts') {
+    if (type === 'ts') {
+      return Prism.highlight(
+        this.tsCode()!,
+        Prism.languages['typescript'],
+        'typescript'
+      );
+    } else if (type === 'html') {
+      return Prism.highlight(
+        this.htmlCode()!,
+        Prism.languages['markup'],
+        'markup'
+      );
+    } else if (type === 'scss') {
+      return Prism.highlight(this.scssCode()!, Prism.languages['scss'], 'scss');
+    } else {
+      return '';
     }
   }
 }
