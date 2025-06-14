a7791b485eb3dd799837520e9f4e448aa775d982
Sanjib Kumar Mandal <sanjib0248@gmail.com>
2025-06-14 16:54:17 +0530

    Auto-generated commit

---
+++ chunk-GO52V7R2.js
@@ -0,0 +1,17 @@
+import{a as c}from"./chunk-AOOLJF6A.js";import{f as l,h as d,z as g}from"./chunk-K5VWRVRZ.js";import"./chunk-3GFDRZ37.js";import{Ib as r,Sa as a,bb as s,mb as o,ub as e,vb as t,wb as i}from"./chunk-KDFRMSR3.js";var b=class m{htmlCode=`
+    <kage-progress-bar></kage-progress-bar>
+    <kage-progress-bar [value]="30" mode="determinate"></kage-progress-bar>
+    <kage-progress-bar mode="buffer" [value]="20"></kage-progress-bar>
+  `;tsCode=`
+  import { Component } from '@angular/core';
+  import { KageProgressBar } from 'kage-ui';
+  ...
+
+  @Component({
+    selector: 'app-example',
+    imports: [ KageProgressBar, ... ],
+    templateUrl: '...',
+    styleUrl: '...',
+  })
+  export class AppExampleComponent {}
+  `;static \u0275fac=function(n){return new(n||m)};static \u0275cmp=s({type:m,selectors:[["app-progress-bar-preview"]],decls:40,vars:6,consts:[[1,"page-tag"],[1,"component"],["label","Library",3,"href"],["label","Progress Bar",3,"href"],[1,"description"],[3,"htmlCode","tsCode"],["mode","determinate",3,"value"],["mode","buffer",3,"value"],[1,"two-col-table"],[1,"two-col-table-head"],[1,"two-col-table-body"],[1,"each"]],template:function(n,p){n&1&&(e(0,"p",0)(1,"span",1),r(2,"Component"),t()(),e(3,"h1"),r(4,"Progress Bar"),t(),e(5,"kage-bread-crumbs"),i(6,"kage-bread-crumb",2)(7,"kage-bread-crumb",3),t(),e(8,"p",4),r(9,"The Progress Bar component visually represents task progress in a linear format. It supports determinate and indeterminate modes, custom colors, and dynamic value updates."),t(),e(10,"app-code-preview",5),i(11,"kage-progress-bar")(12,"kage-progress-bar",6)(13,"kage-progress-bar",7),t(),e(14,"h3"),r(15,"Input"),t(),e(16,"p",4),r(17,"List of Input Parameters."),t(),e(18,"div",8)(19,"div",9)(20,"p"),r(21,"Name"),t(),e(22,"p"),r(23,"Default Value | Type"),t()(),e(24,"div",10)(25,"div",11)(26,"p"),r(27,"mode"),t(),e(28,"p"),r(29,"'determinate' | 'buffer' | 'indeterminate'. Default 'indeterminate'"),t()(),e(30,"div",11)(31,"p"),r(32,"value"),t(),e(33,"p"),r(34,"Type `number`. Default 0"),t()(),e(35,"div",11)(36,"p"),r(37,"color"),t(),e(38,"p"),r(39,"Type `string`. Default 'var(--color-primary)'"),t()()()()),n&2&&(a(6),o("href","/library"),a(),o("href","/library/progress-bar"),a(3),o("htmlCode",p.htmlCode)("tsCode",p.tsCode),a(2),o("value",30),a(),o("value",20))},dependencies:[c,l,d,g],styles:["h1[_ngcontent-%COMP%]{margin-bottom:10px}.description[_ngcontent-%COMP%]{color:var(--color-medium);margin:10px 0}h3[_ngcontent-%COMP%]{color:color-mix(in srgb,var(--color-dark) 80%,var(--color-light) 20%);margin:10px 0}app-code-preview[_ngcontent-%COMP%]   kage-progress-bar[_ngcontent-%COMP%]{margin-bottom:20px;display:block}"]})};export{b as ProgressBarPreviewComponent};
---
+++ chunk-S3NK7E6M.js
@@ -0,0 +1,20 @@
+import{a as x}from"./chunk-AOOLJF6A.js";import{M as v,f as C,h as g}from"./chunk-K5VWRVRZ.js";import"./chunk-3GFDRZ37.js";import{Ib as n,Jb as r,Sa as i,Tb as p,Ub as a,bb as s,mb as d,ub as e,vb as t,wb as c}from"./chunk-KDFRMSR3.js";var f=class l{tsCode=`
+  import { Component } from '@angular/core';
+  import { KageOrdinatePipe } from 'kage-ui';
+  ...
+
+  @Component({
+    selector: 'app-example',
+    imports: [ KageOrdinatePipe, ... ],
+    templateUrl: '...',
+    styleUrl: '...',
+  })
+  export class AppExampleComponent {}
+  `;htmlCode=`
+    <p>{{ 1 | ordinate }}</p>    
+    <p>{{ 2 | ordinate }}</p>    
+    <p>{{ 3 | ordinate }}</p>    
+    <p>{{ 4 | ordinate }}</p>    
+    <p>{{ 11 | ordinate }}</p>   
+    <p>{{ 23 | ordinate }}</p>   
+  `;scssCode="";static \u0275fac=function(o){return new(o||l)};static \u0275cmp=s({type:l,selectors:[["app-ordinate-preview"]],decls:29,vars:23,consts:[[1,"page-tag"],[1,"pipe"],["label","Library",3,"href"],["label","Ordinate Pipe",3,"href"],[1,"description"],[3,"htmlCode","tsCode","scssCode"]],template:function(o,m){o&1&&(e(0,"p",0)(1,"span",1),n(2,"Pipe"),t()(),e(3,"h1"),n(4,"Ordinate Pipe"),t(),e(5,"kage-bread-crumbs"),c(6,"kage-bread-crumb",2)(7,"kage-bread-crumb",3),t(),e(8,"p",4),n(9,"Pipe Behavior and Input Breakdown"),t(),e(10,"app-code-preview",5)(11,"p"),n(12),p(13,"ordinate"),t(),e(14,"p"),n(15),p(16,"ordinate"),t(),e(17,"p"),n(18),p(19,"ordinate"),t(),e(20,"p"),n(21),p(22,"ordinate"),t(),e(23,"p"),n(24),p(25,"ordinate"),t(),e(26,"p"),n(27),p(28,"ordinate"),t()()),o&2&&(i(6),d("href","/library"),i(),d("href","/library/ordinate"),i(3),d("htmlCode",m.htmlCode)("tsCode",m.tsCode)("scssCode",m.scssCode),i(2),r(a(13,11,1)),i(3),r(a(16,13,2)),i(3),r(a(19,15,3)),i(3),r(a(22,17,4)),i(3),r(a(25,19,11)),i(3),r(a(28,21,23)))},dependencies:[x,C,g,v],styles:["h1[_ngcontent-%COMP%]{margin-bottom:10px}.description[_ngcontent-%COMP%]{color:var(--color-medium);margin:10px 0}h3[_ngcontent-%COMP%]{color:color-mix(in srgb,var(--color-dark) 80%,var(--color-light) 20%);margin:10px 0}"]})};export{f as OrdinatePreviewComponent};
