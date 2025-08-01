e609e024caa0348db895d3e081d47231e1f5acd4
Sanjib Kumar Mandal <sanjib.mandal@stockedge.com>
2025-08-01 17:06:05 +0530

    package updated

--- projects/kage-ui/src/components/bread-crumbs/bread-crumbs.component.ts
+++ projects/kage-ui/src/components/bread-crumbs/bread-crumbs.component.ts
@@ -2,16 +2,19 @@ import {
   ChangeDetectionStrategy,
   Component,
   ContentChildren,
+  inject,
   input,
   QueryList,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { KageBreadCrumb } from '../bread-crumb/bread-crumb.component';
 import { KageIcon } from '../icon/icon.component';
+import { KageRippleDirective } from "../../directives/directives";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'kage-bread-crumbs',
-  imports: [KageIcon, RouterLink],
+  imports: [KageIcon, KageRippleDirective, RouterLink],
   templateUrl: './bread-crumbs.component.html',
   styleUrl: './bread-crumbs.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +24,10 @@ export class KageBreadCrumbs {
   iconDepth = input<number>();
   @ContentChildren(KageBreadCrumb)
   breadCrumps!: QueryList<KageBreadCrumb>;
+
+  private location = inject(Location);
+
+  backToPreviousPage() {
+    this.location.back()
+  }
 }
