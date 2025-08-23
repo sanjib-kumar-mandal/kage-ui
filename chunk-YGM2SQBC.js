4c9b3fbce4fa5e24f95dbc8cbe73de79da8595c7
Sanjib Kumar Mandal <sanjib0248@gmail.com>
2025-08-23 18:50:41 +0530

    Auto-generated commit

---
+++ chunk-YGM2SQBC.js
@@ -0,0 +1,13 @@
+import{a as d}from"./chunk-E4LTLPYW.js";import{g as c,k as l,o as g}from"./chunk-F6EKI6RD.js";import"./chunk-742PDQ6L.js";import{Ga as a,Ra as s,ab as n,jb as e,kb as i,lb as m,zb as r}from"./chunk-TROVTPPJ.js";var v=class p{htmlCode='<img style="width: 200px; height: 160px;" kageImageView src="https://images.pexels.com/photos/8563018/pexels-photo-8563018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">';scssCode="";tsCode=`
+  import { Component } from '@angular/core';
+  import { KageImageViewDirective } from 'kage-ui';
+  ...
+
+  @Component({
+    selector: 'app-example',
+    imports: [ KageImageViewDirective, ... ],
+    templateUrl: '...',
+    styleUrl: '...',
+  })
+  export class AppExampleComponent {}
+  `;static \u0275fac=function(t){return new(t||p)};static \u0275cmp=s({type:p,selectors:[["app-image-view-directive-preview"]],decls:13,vars:4,consts:[[1,"page-tag"],[1,"directive"],[3,"iconDepth"],["href","/library","label","Library"],["href","/library/image-view","label","Image view Directive"],[1,"description"],[3,"htmlCode","scssCode","tsCode"],[1,"image"],["kageImageView","","src","https://images.pexels.com/photos/8563018/pexels-photo-8563018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","alt","",2,"width","200px","height","160px"]],template:function(t,o){t&1&&(e(0,"p",0)(1,"span",1),r(2,"Directive"),i()(),e(3,"h1"),r(4,"Image view Directive"),i(),e(5,"kage-bread-crumbs",2),m(6,"kage-bread-crumb",3)(7,"kage-bread-crumb",4),i(),e(8,"p",5),r(9,"The Kage-UI Image Preview Directive enables fullscreen image viewing on click. It detects the src of the target image element and opens it in an overlay with backdrop and close controls\u2014ideal for galleries, thumbnails, or any image needing a zoom-in experience in Angular apps."),i(),e(10,"app-code-preview",6)(11,"div",7),m(12,"img",8),i()()),t&2&&(a(5),n("iconDepth",3),a(5),n("htmlCode",o.htmlCode)("scssCode",o.scssCode)("tsCode",o.tsCode))},dependencies:[d,c,g,l],styles:["h1[_ngcontent-%COMP%]{margin-bottom:10px}.description[_ngcontent-%COMP%]{color:var(--color-medium);margin:10px 0}h3[_ngcontent-%COMP%]{color:color-mix(in srgb,var(--color-dark) 80%,var(--color-light) 20%);margin:10px 0}"]})};export{v as ImageViewDirectivePreviewComponent};
